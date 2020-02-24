3a57017ed2f9ee65ca5d94fec358173c5c596f22
Beza Sirak <sirak.bezawit@gmail.com>
2020-02-24 04:35:22 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -11,7 +11,7 @@ def gross_for_director(director_data)
   row = 0
   total = 0 
   
-  director_data[:movies].validates_each { |movie|
+  director_data[:movies].each { |movie|
     total +=  movie[:worldwide_gross ]
    
     
